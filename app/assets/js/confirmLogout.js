bd2737d164eac30c287582bf7bed6dddc7996609
Kim <kim830119@gmail.com>
2021-09-27 17:33:20 +0800

    add logoutBtn function

---
+++ app/assets/js/confirmLogout.js
@@ -0,0 +1,18 @@
+let blackOverlay = document.getElementById('blackOverlay');
+let logoutConfirm = document.getElementById('logoutConfirm');
+let closeBtn = document.getElementById('close');
+
+let logoutBtn = document.getElementById('logoutBtn');
+logoutBtn.addEventListener('click', function(){
+    blackOverlay.style.display = 'block';
+    logoutConfirm.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    blackOverlay.style.display = 'none';
+    logoutConfirm.style.display = 'none';
+})
+
+
+
+
