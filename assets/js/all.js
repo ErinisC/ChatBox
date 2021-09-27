1f72d0e98f4dc02bb0888a70af0eddf9fe3ae4be
Kim <kim830119@gmail.com>
2021-09-27 17:32:37 +0800

    Update 2021-09-27T09:32:35.417Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,18 @@
 "use strict";
 
 console.log('hello');
+"use strict";
+
+var blackOverlay = document.getElementById('blackOverlay');
+var logoutConfirm = document.getElementById('logoutConfirm');
+var closeBtn = document.getElementById('close');
+var logoutBtn = document.getElementById('logoutBtn');
+logoutBtn.addEventListener('click', function () {
+  blackOverlay.style.display = 'block';
+  logoutConfirm.style.display = 'block';
+});
+closeBtn.addEventListener('click', function () {
+  blackOverlay.style.display = 'none';
+  logoutConfirm.style.display = 'none';
+});
 //# sourceMappingURL=all.js.map
