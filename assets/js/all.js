e38b43ca36355b392aef3150d71d69adeda7b413
Kim <kim830119@gmail.com>
2021-09-28 15:21:42 +0800

    Update 2021-09-28T07:21:41.484Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,6 +3,17 @@
 console.log('hello');
 "use strict";
 
+var groupName = document.getElementById('groupName');
+var changeName = document.getElementById('changeName');
+var cancelBtn = document.getElementById('cancelBtn');
+groupName.addEventListener('click', function () {
+  changeName.style.display = 'block';
+});
+cancelBtn.addEventListener('click', function () {
+  changeName.style.display = 'none';
+});
+"use strict";
+
 var blackOverlay = document.getElementById('blackOverlay');
 var logoutConfirm = document.getElementById('logoutConfirm');
 var closeBtn = document.getElementById('close');
