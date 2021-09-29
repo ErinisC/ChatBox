ede43faf1e01b6ab5d9ee0466ffc3d255f4efce7
Kim <kim830119@gmail.com>
2021-09-29 22:17:24 +0800

    Update 2021-09-29T14:17:23.472Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -31,32 +31,31 @@ $(function () {
       return 0;
     } else {
       for (var j = 0; j < displayLength; j++) {
-        $("#img" + j).attr("src", images[j + 1 + index].src);
+        $("#img" + j).attr("src", images[j + index * displayLength].src);
       }
 
       index++;
     }
   });
-});
-$(function () {
-  $(".itemWrap a").on('mouseenter', function () {
-    var imgSrc = $(this).find('img').attr('src');
-    $(this).siblings('a').find('avatar').attr('src', imgSrc);
+  var NUM_PIC_PER_ROW = 4; // Events
+
+  $(".itemWrap img").on("mouseenter", function () {
+    var imgSrc = $(this).attr("id").substr(3);
+    var finalSrc = parseInt(imgSrc) + index * NUM_PIC_PER_ROW;
+    console.log('index:' + index);
+    console.log('imgSrc:' + imgSrc);
+    console.log('FinalSrc:' + finalSrc);
+    $(".previewAvatar").attr("src", "./assets/images/avatar" + finalSrc + '.png');
   });
-}); // $(document).ready(function(){
-//     $("#img1").click(function(){
-//           $("#avatar1").attr("src","./assets/images/avatar.png");
-//     });
-//     $("#img2").click(function(){
-//           $("#avatar2").attr("src","./assets/images/avatar (2)");
-//     });
-//     $("#img3").click(function(){
-//           $("#avatar3").attr("src","./assets/images/avatar (3)");
-//     });
-//     $("#img4").click(function(){
-//           $("#avatar4").attr("src","./assets/images/avatar (4)");
-//     });
-// });
+  $("#imgs img").on("mouseenter", function () {
+    var imgSrc = $(this).attr("id").substr(3);
+    $(".previewAvatar").attr("src", "./assets/images/avatar" + imgSrc + '.png');
+  });
+}); //                      index
+// [1]  [2] [3] [4]     
+// 1    2   3   4       0
+// 5   6   7   8       1 * 4
+// 9   10   11   12       2 * 4
 "use strict";
 
 var groupName = document.getElementById('groupName');
@@ -82,4 +81,24 @@ closeBtn.addEventListener('click', function () {
   blackOverlay.style.display = 'none';
   logoutConfirm.style.display = 'none';
 });
+"use strict";
+
+// 點擊按鈕
+var moveLeft = document.getElementById("moveLeft");
+var moveRight = document.getElementById("moveRight");
+var moveUp = document.getElementById("moveUp");
+var moveDown = document.getElementById("moveDown"); // 使用者移動角色
+
+var moveAvatarUser = document.getElementById("moveAvatarUser");
+var a = 0;
+
+moveLeft.onclick = function () {
+  a = a - 120;
+  moveAvatarUser.style.marginLeft = a + 'px';
+};
+
+moveRight.onclick = function () {
+  a = a + 120;
+  moveAvatarUser.style.marginLeft = a + 'px';
+};
 //# sourceMappingURL=all.js.map
